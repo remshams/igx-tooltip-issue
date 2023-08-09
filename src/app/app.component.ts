7d000de10f5ff7edc41961993225adcc5e03ec87
Mathias Remshardt <remshams@gmail.com>
2023-08-09 14:00:30 +0200

    adds igx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'igx-chart-tooltip';
-}
+export class AppComponent {}
